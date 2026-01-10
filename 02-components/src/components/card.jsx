1f11aaf49cc806c66cfd471e8221703be4795be4
Aman Singh Tomar <amansinghtomar772005@gamil.com>
2026-01-10 17:04:25 +0530

    props

---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div className='parent'>
+      <div className='card'>
+        <h1>Aman Singh Tomar</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, aliquam laborum maxime est beatae cumque blanditiis non eius consequatur minima molestiae sapiente ipsam reprehenderit recusandae nobis. Illo labore deserunt incidunt!</p>
+        <button>View</button>
+      </div>
+    </div>
+  )
+}
+
+export default card
