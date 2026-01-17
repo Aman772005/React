9cc8415beca91dd64a977b3d4d3be137719e1ce1
Aman Singh Tomar <amansinghtomar772005@gmail.com>
2026-01-17 17:23:49 +0530

    mini project using props

--- 04-project/src/App.jsx
+++ 04-project/src/App.jsx
@@ -2,12 +2,64 @@ import React from 'react'
 import Card from './Components/Card'
 
 const App = () => {
-  return (
-    <div>
-      <Card/>
-      // there is 
+
+  const data = [
+    {
+      companyName: "PixelCraft Studio",
+      post: "Senior UI/UX Designer",
+      tag1: "Part Time",
+      tag2: "Senior Level",
+      pay: "$20/hr",
+      location: "Mumbai, India"
+    },
+    {
+      companyName: "CodeNest Labs",
+      post: "Frontend Developer",
+      tag1: "Full Time",
+      tag2: "Mid Level",
+      pay: "$25/hr",
+      location: "Bangalore, India"
+    },
+    {
+      companyName: "DesignHive",
+      post: "Junior UI Designer",
+      tag1: "Internship",
+      tag2: "Entry Level",
+      pay: "$10/hr",
+      location: "Delhi, India"
+    },
+    {
+      companyName: "TechWave Solutions",
+      post: "Product Designer",
+      tag1: "Remote",
+      tag2: "Senior Level",
+      pay: "$30/hr",
+      location: "Remote, India"
+    },
+    {
+      companyName: "Appify Corp",
+      post: "UX Researcher",
+      tag1: "Contract",
+      tag2: "Mid Level",
+      pay: "$22/hr",
+      location: "Pune, India"
+    }
+  ]
+
+  console.log(data);
+
+  return(
+    <div className='parent'>
+      {data.map(function(elem,idx){
+        return 
+          <Card company = {elem.companyName} post ={elem.post} tag1 = {elem.tag1} tag2 ={elem.tag2} pay = {elem.pay} location ={elem.location}/>
+      })}
     </div>
   )
-}
+
+};
+
+
+
 
 export default App
--- 04-project/src/Components/Card.jsx
+++ 04-project/src/Components/Card.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
+
+  console.log(props.company);
+  
+
   return (
-    <div>
-      <h1>hii</h1>
+    <div className='main'>
+      <div className="top">
+        <div>
+          <img src="https://brandemia.org/contenido/subidas/2022/11/tipografia-y-paleta-de-color.png" alt="" />
+        </div>
+        <div>
+          <h4>Save</h4>
+        </div>
+      </div>
+
+      <div className="center">
+        <h2>{props.company}</h2>
+        <p>5 days ago</p>
+        <h1>{props.post}</h1>
+        <button>{props.tag1}</button>
+        <button>{props.tag2}</button>
+      </div>
+
+      <div className="bottom">
+        <h3>{props.pay}</h3>
+        <p>{props.location}</p>
+        <button>Apply Now</button>
+      </div>
     </div>
   )
 }
