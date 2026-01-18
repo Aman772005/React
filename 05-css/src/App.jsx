1113872e2e37feee4196470b2c22baa998890089
Aman Singh Tomar <amansinghtomar772005@gmail.com>
2026-01-18 16:33:43 +0530

    05 - css

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Button from "./Components/Button/Button";
+
+import Header from './Components/header/Header'
+
+const App = () => {
+  return (
+    <div>
+
+      <Button />
+
+      <Header />
+      
+    </div>
+  )
+}
+
+export default App
