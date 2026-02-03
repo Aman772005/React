d101a1480ecad1a38dd2d0a31ad61a770aac43ff
Aman772005 <amansinghtomar772005@gmail.com>
2026-02-03 20:10:14 +0530

    two way binding and form is completed

--- 13-notes App project/src/App.jsx
+++ 13-notes App project/src/App.jsx
@@ -1,31 +1,41 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const word = () =>{
-    const[title,setTitle]= useState('')
-    setTitle('aman')
-  }
-  const form = (e) =>{
-    e.preventDefault()
-    console.log("form not reload")
-  }
-  return (
-    <div className='h-screen bg-black text-white'> 
 
-      <form onSubmit={(e)=>{
+  const [title, setTitle] = useState('')
+  const [detail , setDetail] = useState('')
+
+  const form = (e) => {
+    e.preventDefault()
+    console.log("this is heading : ",title)
+    console.log("this is notes : ",detail)
+  }
+
+  return (
+    <div className='h-screen bg-black text-white'>
+
+      <form onSubmit={(e) => {
         form(e)
       }} className="flex flex-col p-10 gap-5">
-        <input 
-        onChange={word}
-        type='text'
-        placeholder='Heading'
-        className='border-2 rounded py-2 px-5'
+        <input
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value)
+          }}
+          type='text'
+          placeholder='Heading'
+          className='border-2 rounded py-2 px-5'
         />
-        <textarea 
-        placeholder='Enter notes' 
-        name="" 
-        id=""
-        className='border-2 rounded h-20 px-5 py-2'>
+        <textarea
+          placeholder='Enter notes'
+          name=""
+          id=""
+          className='border-2 rounded h-20 px-5 py-2'
+          value={detail}
+          onChange={(e)=>{
+            setDetail(e.target.value)
+          }}
+          >
         </textarea>
         <button className='bg-white text-black px-5 py-2 rounded'>Add Notes</button>
       </form>
