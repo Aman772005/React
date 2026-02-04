d2271bfeac9f50923adf254c9e3fa3f59465d86d
Aman772005 <amansinghtomar772005@gmail.com>
2026-02-04 20:11:20 +0530

    Notes App

--- 13-notes App project/src/App.jsx
+++ 13-notes App project/src/App.jsx
@@ -3,16 +3,35 @@ import React, { useState } from 'react'
 const App = () => {
 
   const [title, setTitle] = useState('')
-  const [detail , setDetail] = useState('')
+  const [detail, setDetail] = useState('')
+
+  const [task, setTask] = useState([])
 
   const form = (e) => {
     e.preventDefault()
-    console.log("this is heading : ",title)
-    console.log("this is notes : ",detail)
+
+    const copyTask = [...task];
+
+    copyTask.push({ title, detail })
+
+    setTask(copyTask)
+    
+
+
+
+    setTitle('')
+    setDetail('')
+  }
+
+  const notedelete = (idx) =>{
+     const copyTask = [...task];
+     copyTask.splice(idx,1)
+     setTask(copyTask)
+    
   }
 
   return (
-    <div className='h-screen bg-black text-white'>
+    <div className='h-screen bg-black text-white '>
 
       <form onSubmit={(e) => {
         form(e)
@@ -32,13 +51,25 @@ const App = () => {
           id=""
           className='border-2 rounded h-20 px-5 py-2'
           value={detail}
-          onChange={(e)=>{
+          onChange={(e) => {
             setDetail(e.target.value)
           }}
-          >
+        >
         </textarea>
         <button className='bg-white text-black px-5 py-2 rounded'>Add Notes</button>
       </form>
+      <div className='lg:w-1/2 lg:border-l-2 p-10 flex align-middle gap-5'>
+
+        {task.map(function (elem, idx) {
+          return <div key={idx} className='h-50 w-40 bg-white text-black rounded-2xl '>
+            <h1>{elem.title}</h1>
+            <p>{elem.detail}</p>
+            <button onClick={()=>{
+              notedelete(idx)
+              }}>Delete</button>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
