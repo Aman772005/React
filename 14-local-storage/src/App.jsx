13784ad08505a153ca42577c388108e0ce30b58c
Aman772005 <amansinghtomar772005@gmail.com>
2026-02-05 17:15:09 +0530

    local storage
    learn about clear , remove , get , set , JSON.parse,JSON.stansify

---
+++ 14-local-storage/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const App = () => {
+  //  localStorage.clear()
+  // sessionStorage.clear()
+  // localStorage.setItem('user','aman')
+//  const user = localStorage.getItem('user')
+//  console.log(user);
+// localStorage.setItem('age',18)
+// const age = localStorage.getItem('age')
+// console.log(age)
+// localStorage.removeItem('user')
+// localStorage.removeItem('age')
+ 
+const user = {
+  username : 'aman',
+  age:18,
+  city : 'gwalior'
+}
+// localStorage.setItem('user' , user)
+
+// localStorage.setItem('user' , JSON.stringify(user))
+
+// const usera = JSON.parse(localStorage.getItem('user'))
+// console.log(usera)
+
+  return (
+    <div>
+      div
+    </div>
+  )
+}
+
+export default App
