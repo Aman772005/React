1c595f269b111787fd8639e15691f170c2c54d16
Aman772005 <amansinghtomar772005@gmail.com>
2026-02-14 12:35:05 +0530

    Gallery UI , Axios API

--- 17-Gallery/src/App.jsx
+++ 17-Gallery/src/App.jsx
@@ -1,40 +1,57 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Card from './components/Card';
 
 const App = () => {
 
-  const [userdata ,setUserData] = useState([]);
+  const [userdata, setUserData] = useState([]);
+  const [Index, setIndex] = useState(1);
 
-  const getdata = async () =>{
-  const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=25');
+  const getdata = async () => {
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${Index}&limit=10`);
 
-  setUserData(response.data)
-  console.log(response.data)
-}
+    setUserData(response.data);
+  }
 
-useEffect(function(){
-  getdata()
-},[])
+  useEffect(function () {
+    getdata()
+  }, [Index])
 
-let printuserdata = <h3 className='text-gray-400 '>No user avilable</h3>
-if(userdata.length>0){
-  printuserdata = userdata.map((elem,idx)=>{
-    return <div>
-      <a href={elem.url} target='_blank'>
-        <div className='h-40 w-45 bg-white overflow-hidden rounded-xl'>
-      <img className='h-full  w-full object-cover  ' src={elem.download_url}/>
-    </div>
-    <h2 className='font-bold text-lg'>{elem.author}</h2>
-      </a>
-    </div>
-  })
-}
+  let printuserdata = <h3 className='text-gray-400 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 '>Loading....</h3>
+  if (userdata.length > 0) {
+    printuserdata = userdata.map((elem, idx) => {
+      return <div key={idx}>
+        <Card elem = {elem}/>
+      </div>
+    })
+  }
 
   return (
     <div className='bg-black h-screen text-white overflow-auto'>
       {/* <button onClick={getdata}>Get Data</button> */}
       <div className='flex flex-wrap gap-4 '>
         {printuserdata}
+      </div>
+      <div className='flex justify-self-center gap-5 p-5'>
+        <button
+          onClick={() => {
+            if (Index > 1) {
+              setUserData([])
+              setIndex(Index - 1);
+            }
+          }}
+          className='bg-amber-200 px-2 py-1 rounded-xl text-black font-bold cursor-pointer'>
+          Prev
+        </button>
+        <h4>Page {Index}</h4>
+        <button
+          onClick={() => {
+            setUserData([])
+            setIndex(Index + 1)
+          }}
+          className='bg-amber-200 px-2 py-1 rounded-xl text-black font-bold cursor-pointer'>
+          Next
+        </button>
       </div>
     </div>
   )
